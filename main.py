d4d62a91f272b6e313a97286b55e5a7c45f5dd94
Clara Hoffmann <claracharlotteh@web.de>
2023-04-05 16:53:15 +0200

    restructure front page

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(layout="wide")
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.app_logo import add_logo
 
-st.markdown("""
-<style>
-.big-font {
-    font-size:70px !important;
-}
-</style>
-""", unsafe_allow_html=True)
+st.set_page_config(
+    layout="wide",
+)
+#add_logo("pages/images/pycon_pydata_2023-min.png", height=10)
 
 
-st.markdown("""
-<style>
-.medium-font {
-    font-size:50px !important;
-}
-</style>
-""", unsafe_allow_html=True)
+col1, col2 = st.columns([6,5])
 
+with col1: 
+    
+    
+    st.write('#')
+    st.write('#')
+    st.write('#')
+    st.markdown('''# Honey, I broke the PyTorch model''')
+    st.markdown('''# 🍯😊⛏️🐍🔥🧮''')
 
-image_directory = "pages/images/pycon_pydata_logo.png"
-image = Image.open(image_directory)
-st.image(image)
-st.markdown('<p class="big-font">Honey, I broke the PyTorch model</p>', unsafe_allow_html=True)
-st.markdown('<p class="big-font">🍯😊⛏️🐍🔥🧮</p>', unsafe_allow_html=True)
-st.markdown('<p class="medium-font">Clara Hoffmann - PyCon DE & PyData Berlin 2023</p>', unsafe_allow_html=True)
+    st.write('#')
+
+    st.markdown('''# Clara Hoffmann''')
+    st.markdown('''### 17.-19.04.2023''')
+    st.markdown('''### PyCon DE & PyData Berlin 2023''')
+
+with col2:
+    st.write('#')
+    st.write('#')
+    st.write('#')
+    st.write('#')
+    st.write('#')
+    st.write('#')
+    st.write('#')
+    image = Image.open("pages/images/pycon_pydata_logo.png")
+    st.image(image, width = 600)
+
