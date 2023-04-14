69ae598052000a65d0c6287ec037af484bc9869c
Clara Hoffmann <claracharlotteh@web.de>
2023-04-14 11:27:27 +0200

    fix hooks

--- pages/torch_examples/run_ww.py
+++ pages/torch_examples/run_ww.py
@@ -1,46 +1,44 @@
+"""Run weightwatcher on trained PyTorch model"""
+import argparse
+from pathlib import Path
+
 import torch
 import weightwatcher as ww
-from pathlib import Path
-import argparse
 from loguru import logger
-import pandas as pd
 
 
 def run_weightwatcher(ckpt_path: Path, output_file_path: Path) -> None:
+    """Run weightwatcher on PyTorch checkpoint"""
 
-    logger.info(f'Reading checkpoint {ckpt_path} ...')
-    ckpt = torch.load(ckpt_path,map_location=torch.device('cpu') )
-    print(ckpt['state_dict'])
-    logger.info(f'... successfully loaded')
+    logger.info(f"Reading checkpoint {ckpt_path} ...")
+    ckpt = torch.load(ckpt_path, map_location=torch.device("cpu"))
+    print(ckpt["state_dict"])
+    logger.info("... successfully loaded")
 
-    logger.info(f'Start weightwatcher analysis ...')
-    watcher = ww.WeightWatcher(model=ckpt['state_dict'])
-    results = watcher.analyze()
+    logger.info("Start weightwatcher analysis ...")
+    watcher = ww.WeightWatcher(model=ckpt["state_dict"])
     details = watcher.get_details()
-    logger.info(f'... successfully analyzed')
+    logger.info("... successfully analyzed")
 
-    print(details)
-
-    logger.info(f'Writing output to {output_file_path} ...')
-    details.to_csv(output_file_path, index = False)
-    logger.info(f'... finished writing output')
+    logger.info(f"Writing output to {output_file_path} ...")
+    details.to_csv(output_file_path, index=False)
+    logger.info("... finished writing output")
 
 
 def main():
+    """Run weightwatcher on PyTorch checkpoint"""
     parser = argparse.ArgumentParser(
-                    prog='Train models',
-                    description='Train models with synthetic data and different bugs')
-    
-    parser.add_argument('--ckpt_path', 
-                        type = Path, 
-                        required = True)
-    parser.add_argument('--output_file_path', 
-                        type = Path, 
-                        required = True)
+        prog="Run weightwatcher",
+        description="Run weightwatcher on PyTorch checkpoint",
+    )
 
-    args = parser.parse_args()  
+    parser.add_argument("--ckpt_path", type=Path, required=True)
+    parser.add_argument("--output_file_path", type=Path, required=True)
+
+    args = parser.parse_args()
 
     run_weightwatcher(args.ckpt_path, args.output_file_path)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
